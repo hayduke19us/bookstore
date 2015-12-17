b04fcc95672db99be8c82dc7ca11b448b411699e
hayduke19us <hayduke19us@gmail.com>
2015-12-17 01:17:57 -0600

    add associations between book and publisher, author...add fixtures

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ActiveRecord::Base
   validates :title, :publisher_id, :author_id, presence: true
+
+  belongs_to :publisher
+  belongs_to :author
+
+  has_many :book_formats
+  has_many :book_format_types, through: :book_formats
 end
--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,3 +1,5 @@
 class Publisher < ActiveRecord::Base
   validates :name, presence: true
+
+  has_many :books
 end
